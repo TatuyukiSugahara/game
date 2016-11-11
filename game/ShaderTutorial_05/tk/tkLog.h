21f7a46a4814f57cf365c4e539a78508801d4348
sugahara <a>
2016-11-11 09:33:34 +0900

    add

---
+++ game/ShaderTutorial_05/tk/tkLog.h
@@ -0,0 +1,30 @@
+/*!
+ *@brief		ÉçÉO
+ */
+
+#ifndef _TKLOG_H_
+#define _TKLOG_H_
+#include <stdio.h>
+#include <stdarg.h>
+#include <debugapi.h>
+
+
+	/*!
+	 *@brief	ÉçÉOèoóÕ
+	 */
+	static inline void Log( const char* format, ... )
+	{
+		char log[256];
+		va_list va;
+		va_start( va, format );
+		vsprintf( log, format, va );
+		OutputDebugString( log );
+		va_end( va );
+	}
+
+#ifdef _DEBUG
+	#define TK_LOG( format, ... )	Log(format, __VA_ARGS__)
+#else // _DEBUG
+#define TK_LOG( format, ... )	Log(format, __VA_ARGS__)
+#endif // _DEBUG
+#endif // _TKLOG_H_
